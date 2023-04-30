c81a478c9cafa95e3c6c9c1bb8421b9143693b64
David Antúnez Pérez <david.antunez.perez@dekra.com>
2023-04-30 17:36:06 +0200

    Added activity navigation in SelectTable activity when clicking any table (not giving the table information yet).

--- app/src/main/java/com/example/restaurapp/firestore/SelectTableAdapter.kt
+++ app/src/main/java/com/example/restaurapp/firestore/SelectTableAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.restaurapp.firestore
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.restaurapp.ComListActivity
+import com.example.restaurapp.CreateComActivity
 import com.example.restaurapp.R
 import com.example.restaurapp.SelectTableActivity
 
@@ -25,6 +28,11 @@ class SelectTableAdapter(private val tableList: ArrayList<Table>) :
     override fun onBindViewHolder(holder: SelectTableViewHolder, position: Int) {
         holder.tvNumber.text = tableList[position].number.toString()
         holder.tvInfo.text = tableList[position].info
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, CreateComActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
