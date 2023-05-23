1fafe3a19d9d38897fa23d3d320a27d341005c72
David Antúnez Pérez <david.antunez.perez@dekra.com>
2023-05-23 14:45:30 +0200

    Edit settings Name, Description and Location are now functional.

--- app/src/main/java/com/example/restaurapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/restaurapp/fragments/SettingsFragment.kt
@@ -15,7 +15,9 @@ import androidx.fragment.app.Fragment
 import com.example.restaurapp.R
 import com.example.restaurapp.activities.SignInActivity
 import com.example.restaurapp.databinding.FragmentSettingsBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import java.util.Locale
 
 class SettingsFragment : Fragment() {
@@ -28,6 +30,14 @@ class SettingsFragment : Fragment() {
     private lateinit var languageSpinner: Spinner
 
     private lateinit var sharedPreferences: SharedPreferences
+
+    private lateinit var firestore: FirebaseFirestore
+
+    // FIRE STORE RETRIEVED VALUES
+    private var userImage: String = ""
+    private var userDescription: String = ""
+    private var userName: String = ""
+    private var userLocation: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -59,6 +69,9 @@ class SettingsFragment : Fragment() {
         // Initialize Firebase Auth
         firebaseAuth = FirebaseAuth.getInstance()
 
+        // Initialize Firebase Fire store
+        firestore = FirebaseFirestore.getInstance()
+
         // Sign out button function
         binding.btnLogout.setOnClickListener {
             firebaseAuth.signOut()
@@ -69,6 +82,9 @@ class SettingsFragment : Fragment() {
         // Initialize shared preferences
         sharedPreferences =
             requireContext().getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
+
+        // GET THE USER UID
+        val userUID = sharedPreferences.getString("userUID", "userUID")
 
         // SET UP THE LANGUAGE SPINNER
         languageSpinner = binding.languageSpinner
@@ -87,6 +103,27 @@ class SettingsFragment : Fragment() {
         val languageIndex = currentLanguage?.let { getLanguageIndex(it) } ?: 0
         languageSpinner.setSelection(languageIndex)
 
+        // RETRIEVE THE USER DATA FROM FIREBASE
+        // Retrieve user document from Firestore
+        val userDocumentRef = firestore.collection("users").document(userUID!!)
+        userDocumentRef.get().addOnSuccessListener { documentSnapshot ->
+            if (documentSnapshot.exists()) {
+                val userData = documentSnapshot.data
+                if (userData != null) {
+                    userImage = userData["image"] as? String ?: ""
+                    userDescription = userData["description"] as? String ?: ""
+                    userName = userData["name"] as? String ?: ""
+                    userLocation = userData["location"] as? String ?: ""
+
+                    // Update UI with the retrieved values
+                    updateUIWithUserData()
+                }
+            } else {
+                // Document does not exist
+                Log.d("SettingsFragment", "User document does not exist")
+            }
+        }
+
         // Handle save button click
         binding.saveButton.setOnClickListener {
             saveSettings()
@@ -100,8 +137,24 @@ class SettingsFragment : Fragment() {
 
         saveChangeLang()
 
-        // Save the settings or update the user profile with the entered information
-        // Implement your logic here
+        // Update the user document in Firestore with the entered information
+        val userUID = sharedPreferences.getString("userUID", "userUID")
+        val userDocumentRef = firestore.collection("users").document(userUID!!)
+
+        val userData: MutableMap<String, Any?> = hashMapOf(
+            "name" to name, "description" to description, "location" to location
+        )
+
+        userDocumentRef.update(userData).addOnSuccessListener {
+            // Show success message using Snackbar
+            Snackbar.make(binding.root, "Settings saved successfully", Snackbar.LENGTH_SHORT).show()
+
+
+            // Perform any additional actions or UI updates
+        }.addOnFailureListener { exception ->
+            // Show error message using Snackbar
+            Snackbar.make(binding.root, "Error saving settings", Snackbar.LENGTH_SHORT).show()
+        }
     }
 
     private fun saveChangeLang() {
@@ -140,6 +193,16 @@ class SettingsFragment : Fragment() {
             Locale("es").language -> getString(R.string.spanish_language)
             else -> getString(R.string.english_language) // Set English as the default language
         }
+    }
+
+    private fun updateUIWithUserData() {
+        // Update UI elements with the retrieved user data
+        binding.nameEditText.setText(userName)
+        binding.descriptionEditText.setText(userDescription)
+        binding.locationEditText.setText(userLocation)
+
+        // Load user image using the userImage variable
+        // Implement your logic here
     }
 
     override fun onDestroyView() {
